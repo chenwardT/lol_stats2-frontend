f3af16a31065802bdaff86c862a93d2a0d2bb964
Edward Chen <chenward.T@gmail.com>
2016-01-26 17:07:15 -0500

    Add addChampions action creator. Improve names of constants and functions.

--- src/actions.js
+++ src/actions.js
@@ -1,24 +1,29 @@
 /*
  * action types
  */
-export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
+export const SET_ROLE_FILTER = 'SET_ROLE_FILTER';
+export const ADD_CHAMPIONS = 'ADD_CHAMPIONS';
 
 /*
  * other constants
  */
-export const VisibilityFilters = {
-  ALL:     'ALL',
-  TOP:     'TOP',
-  JUNGLE:  'JUNGLE',
-  MIDDLE:  'MIDDLE',
-  ADC:     'ADC',
-  SUPPORT: 'SUPPORT'
+export const RoleFilters = {
+  SHOW_ALL:     'SHOW_ALL',
+  SHOW_TOP:     'SHOW_TOP',
+  SHOW_JUNGLE:  'SHOW_JUNGLE',
+  SHOW_MIDDLE:  'SHOW_MIDDLE',
+  SHOW_ADC:     'SHOW_ADC',
+  SHOW_SUPPORT: 'SHOW_SUPPORT'
 };
 
 /*
  * action creators
  */
 
-export function setVisibilityFilter(filter) {
-  return { type: SET_VISIBILITY_FILTER, filter };
+export function setRoleFilter(filter) {
+  return { type: SET_ROLE_FILTER, filter };
+}
+
+export function addChampions(champions) {
+  return { type: ADD_CHAMPIONS, champions };
 }
