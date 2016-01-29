f86eff0c0cd45458364ef43bf0afe1f2a039d6e3
Edward Chen <chenward.T@gmail.com>
2016-01-29 18:28:50 -0500

    Add navbar to CoreLayout.

--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react'
 import '../../styles/core.scss'
+import { Link } from 'react-router'
 
 // Note: Stateless/function components *will not* hot reload!
 // react-transform *only* works on component classes.
@@ -13,11 +14,63 @@ import '../../styles/core.scss'
 function CoreLayout ({ children }) {
   return (
     <div className='page-container'>
-      <div className='view-container'>
-        {children}
+      <nav className='navbar navbar-default'>
+        <div className='container-fluid'>
+          {/* Brand and toggle get grouped for better mobile display */}
+          <div className='navbar-header'>
+            <button type='button' className='navbar-toggle collapsed' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1' aria-expanded='false'>
+              <span className='sr-only'>Toggle navigation</span>
+              <span className='icon-bar'></span>
+              <span className='icon-bar'></span>
+              <span className='icon-bar'></span>
+            </button>
+            <a className='navbar-brand brand' href='#'>Snaptrap</a>
+          </div>
+
+          {/* Collect the nav links, forms, and other content for toggling */}
+          <div className='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>
+            <ul className='nav navbar-nav'>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/summoners'>Summoners</Link></li>
+              <li><Link to='/stats'>Champions</Link></li>
+              <li><Link to='/about'>About</Link></li>
+            </ul>
+            <ul className='nav navbar-nav navbar-right'>
+              <li><a href='#'>Social Media</a></li>
+            </ul>
+            <form className='navbar-form navbar-left' role='search'>
+              <div className='form-group'>
+                <input type='text' id='summoner-champion-search' className='form-control' placeholder='Summoner or Champion' />
+                <button className='btn btn-default'><span className='glyphicon glyphicon-search' /></button>
+              </div>
+            </form>
+          </div>{/* /.navbar-collapse */}
+        </div>{/* /.container-fluid */}
+      </nav>
+
+      <div className='row'>
+        <div className='header-container col-sm-12'>
+          <div className='title-and-search-container'>
+            <span className='title'>Snaptrap</span>
+          </div>
+          <div className='patch-status text-center'>
+            <span className='patch-status-txt'>Patch</span>{' '}
+            <span className='patch-status-num'>6.1</span>{' | '}
+            <span className='patch-status-txt'>Champions Analyzed</span>{' '}
+            <span className='patch-status-num'>2,134,459 </span>{' | '}
+            <span className='patch-status-txt'>Matches Aggregated</span>{' '}
+            <span className='patch-status-num'>311,293</span>
+          </div>
+        </div>
+      </div>
+
+      <div className='row'>
+        <div className='view-container col-sm-12'>
+          {children}
+        </div>
       </div>
     </div>
-  )
+)
 }
 
 CoreLayout.propTypes = {
