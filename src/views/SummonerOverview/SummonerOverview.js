877a9efc1481f602a883e5597fb8c0a9da442a76
Edward Chen <chenward.T@gmail.com>
2016-02-01 17:31:41 -0500

    Add placeholder view for /summoners route.

---
+++ src/views/SummonerOverview/SummonerOverview.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SummonerOverview extends Component {
+  render() {
+    return (
+      <div>
+        Summoner Overview
+      </div>
+    )
+  }
+}
