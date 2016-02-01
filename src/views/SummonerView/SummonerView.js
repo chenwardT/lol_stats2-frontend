f367ed4ea35a1d42a082ff090e1242334adc797e
Edward Chen <chenward.T@gmail.com>
2016-02-01 17:32:20 -0500

    Start on initialization of summoner store with fetched data.

--- src/views/SummonerView/SummonerView.js
+++ src/views/SummonerView/SummonerView.js
@@ -2,10 +2,58 @@ import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import classes from './SummonerView.scss'
 
+import { actions as summonerActions } from 'redux/modules/summoner'
 import { Header } from 'components/Summoner/Header'
 import { Match } from 'components/Summoner/Match'
 
 export class SummonerContainer extends Component {
+  constructor(props) {
+    // First, resolve the region + name to the PK of the summoner on the backend
+    super(props)
+    console.log(this.props.location)
+    const location = this.props.location.pathname
+    const region = location.split('/')[2]
+    const name = location.split('/')[3]
+    let pk
+
+    const fetchPKInit = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        'region': region,
+        'name': name
+      })
+    }
+
+    fetch('http://laguz:8001/get_pk/', fetchPKInit)
+      .then((resp) => resp.json())
+      .then((json) => {
+        console.log(`/${json.region}/${json.name}`)
+        pk = json.id
+        console.log(pk)
+        return json
+      })
+      .then((resp) => {
+        // Then use the PK to query the summoner data
+        const fetchMatchesInit = {
+          method: 'GET',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          }
+        }
+        fetch(`http://laguz:8001/summoner-matches/${resp.id}`, fetchMatchesInit)
+          .then((resp) => resp.json())
+          .then((resp) => {
+            console.log(`Got ${resp.results.length} results`)
+            this.props.setMatches({results: resp.results})
+          })
+      })
+  }
+
   render() {
     return (
       <div className='summoner-view-container container-fluid'>
@@ -32,4 +80,4 @@ const mapStateToProps = (state) => ({
 
 })
 
-export default connect(mapStateToProps)(SummonerContainer)
+export default connect(mapStateToProps, summonerActions)(SummonerContainer)
