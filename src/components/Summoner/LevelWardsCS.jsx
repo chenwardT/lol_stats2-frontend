7013b195afc223517f855e3919ad0d9f5ed25a0d
Edward Chen <chenward.T@gmail.com>
2016-02-02 18:00:51 -0500

    Change LevelWardsCS to a class. Use props.

--- src/components/Summoner/LevelWardsCS.jsx
+++ src/components/Summoner/LevelWardsCS.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import classes from 'views/SummonerView/SummonerView.scss'
 
-export const LevelWardsCS = ({match}) => (
-  <div className={classes.lvlCsWards}>
-    <div className={classes.level}>
-      Level 16
-    </div>
-    <div className={classes.cs}>
-      102 (2.7) CS
-    </div>
-    <div className={classes.wards}>
-      Bought 3 wards
-    </div>
-    <div className={classes.killParticipation}>
-      41% Kill Participation
-    </div>
-  </div>
-)
+export default class LevelWardsCS extends Component {
+  render() {
+    const totalMinionsKilled = this.props.participantData.minions_killed +
+      this.props.participantData.neutral_minions_killed
+    const { killParticipation, participantData, matchDuration } = this.props
+
+    return (
+      <div className={classes.lvlCsWards}>
+        <div className={classes.level}>
+          Level {participantData.champ_level}
+        </div>
+        <div className={classes.cs}>
+          {totalMinionsKilled + ' (' + (totalMinionsKilled / (matchDuration / 60)).toFixed(1) + ') CS'}
+        </div>
+        <div className={classes.wards}>
+          Bought {participantData.vision_wards_bought_in_game} wards
+        </div>
+        <div className={classes.killParticipation}>
+          {(killParticipation * 100).toFixed(0)}% Kill Participation
+        </div>
+      </div>
+    )
+  }
+}
