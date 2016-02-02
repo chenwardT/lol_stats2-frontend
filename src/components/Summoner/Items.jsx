1d2d02f5b6ad2bbfaf233b766dd036daeec73a8d
Edward Chen <chenward.T@gmail.com>
2016-02-02 17:09:40 -0500

    Simple keying for Summoner/Items.

--- src/components/Summoner/Items.jsx
+++ src/components/Summoner/Items.jsx
@@ -4,8 +4,8 @@ import classes from 'views/SummonerView/SummonerView.scss'
 
 export const Items = ({match}) => (
   <div className={classes.items}>
-    {Array.from(Array(6).keys()).map(() => (
-    <div className={classes.item}>
+    {Array.from(Array(6).keys()).map((_, idx) => (
+    <div key={idx} className={classes.item}>
       <img src='http://ddragon.leagueoflegends.com/cdn/6.2.1/img/item/1001.png' />
     </div>
       ))}
