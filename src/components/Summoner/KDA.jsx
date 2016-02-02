2b2ec1db9cc97113dd386fb43bf382dd7ed0e7ee
Edward Chen <chenward.T@gmail.com>
2016-02-02 17:10:41 -0500

    Change KDA to a class and use props.

--- src/components/Summoner/KDA.jsx
+++ src/components/Summoner/KDA.jsx
@@ -1,18 +1,37 @@
-import React from 'react'
+import React, { Component, PropTypes } from 'react'
 
 import classes from 'views/SummonerView/SummonerView.scss'
 
-export const KDA = ({match}) => (
-  <div className={classes.kdaSection}>
-    <div className={classes.kda}>
-      <span className={classes.kills}>5</span>
-      {' / '}
-      <span className={classes.deaths}>2</span>
-      {' / '}
-      <span className={classes.assists}>7</span>
-    </div>
-    <div className={classes.kdaRatio}>
-      1.58:1 KDA
-    </div>
-  </div>
-)
+export default class KDA extends Component {
+  static propTypes = {
+    participantData: PropTypes.object
+  };
+
+  getKDARatioText() {
+    const { kills, deaths, assists } = this.props.participantData
+    if (deaths === 0) {
+      return <span className={classes.perfectGame}>Perfect KDA</span>
+    } else {
+      return <span className={classes.kdaText}>{((kills + assists) / deaths).toFixed(2)} KDA</span>
+    }
+  }
+  
+  render() {
+    const { kills, deaths, assists } = this.props.participantData
+
+    return (
+      <div className={classes.kdaSection}>
+        <div className={classes.kda}>
+          <span className={classes.kills}>{kills}</span>
+          {' / '}
+          <span className={classes.deaths}>{deaths}</span>
+          {' / '}
+          <span className={classes.assists}>{assists}</span>
+        </div>
+        <div className={classes.kdaRatio}>
+          {this.getKDARatioText()}
+        </div>
+      </div>
+    )
+  }
+}
