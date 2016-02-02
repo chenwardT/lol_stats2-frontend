4d18a4e8526259f6c25c028bee3d7b16d2e00952
Edward Chen <chenward.T@gmail.com>
2016-02-01 19:29:20 -0500

    Turn Match into a class. Add functionality to get this page's summoner's participant_id.

--- src/components/Summoner/Match.jsx
+++ src/components/Summoner/Match.jsx
@@ -1,25 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import { ChampionAndSpells } from './ChampionAndSpells'
 import { KDA } from './KDA'
 import { LevelWardsCS } from './LevelWardsCS'
 import { Items } from './Items'
 import { Trinket } from './Trinket'
-import { Participants } from './Participants'
+import Participants from './Participants'
 import classes from 'views/SummonerView/SummonerView.scss'
 
-export const Match = ({match}) => (
-  <div className={classes.match}>
-    <div className={classes.matchTitle}>
-      Ranked - 18 hours ago
-    </div>
-    <div className={classes.matchStats}>
-      <ChampionAndSpells />
-      <KDA />
-      <LevelWardsCS />
-      <Items />
-      <Trinket />
-      <Participants />
-    </div>
-  </div>
-)
+export default class Match extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  // Get the participantID of the summoner for this match.
+  // This is used to determine which items to display in the summary view.
+  getParticipantID(participantIdentitySet) {
+    for (let participant of participantIdentitySet) {
+      if (participant.summoner.name === this.props.name) {
+        return participant.participant_id
+      }
+    }
+  }
+
+  render() {
+    const { matchData } = this.props
+    const thisParticipant = this.getParticipantID(matchData.participantidentity_set)
+
+    return (
+      <div className={classes.match}>
+        <div className={classes.matchTitle}>
+          {matchData.queue_type}{' '}{matchData.match_creation}
+        </div>
+        <div className={classes.matchStats}>
+          <ChampionAndSpells participantID={thisParticipant} />
+          <KDA />
+          <LevelWardsCS />
+          <Items />
+          <Trinket />
+          <Participants
+            participantSet={matchData.participant_set}
+            participantIdentitySet={matchData.participantidentity_set}
+          />
+        </div>
+      </div>
+    )
+  }
+}
