e8147548fac6b8910160365029529251a29e4e1f
Edward Chen <chenward.T@gmail.com>
2016-02-02 17:11:52 -0500

    Add clearMatches action and initialize matches to loading string.

--- src/redux/modules/summoner.js
+++ src/redux/modules/summoner.js
@@ -4,23 +4,28 @@ import { createAction, handleActions } from 'redux-actions'
  * Constants
  */
 export const SET_MATCHES = 'SET_MATCHES'
+export const CLEAR_MATCHES = 'CLEAR_MATCHES'
 
 /*
  * Actions
  */
 export const setMatches = createAction(SET_MATCHES)
+export const clearMatches = createAction(CLEAR_MATCHES)
 
 export const actions = {
-  setMatches
+  setMatches,
+  clearMatches
 }
 
 /*
  * Reducer
  */
-const sampleData = require('./summonerSampleData.json')
+// const sampleData = require('./summonerSampleData.json')
 
 export default handleActions({
-  [SET_MATCHES]: (state, {payload}) => (Object.assign({}, state, { matches: payload.results }))
+  [SET_MATCHES]: (state, {payload}) => (Object.assign({}, state, { matches: payload.results })),
+  [CLEAR_MATCHES]: (state, _) => (Object.assign({}, state, { matches: 'loading' }))
 }, {
-  matches: sampleData.results
+  // matches: sampleData.results
+  matches: 'loading'
 })
