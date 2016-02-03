bebc1f1c3d0278a23463ab16d3139eba09fed834
Edward Chen <chenward.T@gmail.com>
2016-02-03 18:28:53 -0500

    Add reducers for setting static data.

--- src/redux/modules/core.js
+++ src/redux/modules/core.js
@@ -4,21 +4,31 @@ import { createAction, handleActions } from 'redux-actions'
  * Constants
  */
 export const SET_STATIC_CHAMPION_DATA = 'SET_STATIC_CHAMPION_DATA'
+export const SET_STATIC_SPELL_DATA = 'SET_STATIC_SPELL_DATA'
+export const SET_STATIC_ITEM_DATA = 'SET_STATIC_ITEM_DATA'
 
 /*
  * Actions
  */
 export const setStaticChampionData = createAction(SET_STATIC_CHAMPION_DATA)
+export const setStaticSpellData = createAction(SET_STATIC_SPELL_DATA)
+export const setStaticItemData = createAction(SET_STATIC_ITEM_DATA)
 
 export const actions = {
-  setStaticChampionData
+  setStaticChampionData,
+  setStaticSpellData,
+  setStaticItemData
 }
 
 /*
  * Reducer
  */
 export default handleActions({
-  [SET_STATIC_CHAMPION_DATA]: (state, {payload}) => (Object.assign({}, { champions: payload }))
+  [SET_STATIC_CHAMPION_DATA]: (state, {payload}) => (Object.assign({}, state, { champions: payload })),
+  [SET_STATIC_SPELL_DATA]: (state, {payload}) => (Object.assign({}, state, { spells: payload })),
+  [SET_STATIC_ITEM_DATA]: (state, {payload}) => (Object.assign({}, state, { items: payload }))
 }, {
-  champions: {}
+  champions: {},
+  spells: {},
+  items: {}
 })
