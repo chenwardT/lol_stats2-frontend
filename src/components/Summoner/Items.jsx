af615efc8959a756537daf5ef3e1a9bd10d0cf10
Edward Chen <chenward.T@gmail.com>
2016-02-03 20:09:25 -0500

    Change Summoner/Items to a class. Use trinket ID to get image.

--- src/components/Summoner/Items.jsx
+++ src/components/Summoner/Items.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { Component, PropTypes } from 'react'
 
 import classes from 'views/SummonerView/SummonerView.scss'
 
-export const Items = ({match}) => (
-  <div className={classes.items}>
-    {Array.from(Array(6).keys()).map((_, idx) => (
-    <div key={idx} className={classes.item}>
-      <img src='http://ddragon.leagueoflegends.com/cdn/6.2.1/img/item/1001.png' />
-    </div>
-      ))}
-  </div>
-)
+export default class Items extends Component {
+  static propTypes = {
+    participantData: PropTypes.object.isRequired
+  };
+
+  render() {
+    const { participantData } = this.props
+    const item_ids = [participantData.item0, participantData.item1,
+                      participantData.item2, participantData.item3,
+                      participantData.item4, participantData.item5]
+    return (
+      <div className={classes.items}>
+        {item_ids.filter(item => item !== 0).map((item, idx) => (
+          <div key={idx} className={classes.item}>
+            <img src={`http://ddragon.leagueoflegends.com/cdn/6.2.1/img/item/${item}.png`}/>
+          </div>
+        ))}
+      </div>
+    )
+  }
+}
