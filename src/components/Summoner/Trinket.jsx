f1ee1c00144249c755bcece4dc8ed0ad3487b627
Edward Chen <chenward.T@gmail.com>
2016-02-03 19:04:51 -0500

    Use trinket's ID to get correct image in Summoner/Trinket.

--- src/components/Summoner/Trinket.jsx
+++ src/components/Summoner/Trinket.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 import classes from 'views/SummonerView/SummonerView.scss'
 
-export const Trinket = ({match}) => (
+export const Trinket = ({participantData}) => (
   <div className={classes.trinket}>
-    <img src='http://ddragon.leagueoflegends.com/cdn/6.2.1/img/item/3341.png' />
+    <img src={`http://ddragon.leagueoflegends.com/cdn/6.2.1/img/item/${participantData.item6}.png`} />
   </div>
 )
